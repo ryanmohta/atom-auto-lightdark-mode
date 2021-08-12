0e6220587c0efc688853f241d145ff52e7ada52f
Ryan Mohta <ryanmohta@gmail.com>
2021-08-12 02:30:15 -0700

    Initial commit

---
+++ spec/atom-auto-lightdark-mode-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomAutoLightdarkModeView from '../lib/atom-auto-lightdark-mode-view';
+
+describe('AtomAutoLightdarkModeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
