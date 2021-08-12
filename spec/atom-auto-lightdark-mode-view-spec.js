6680cb16de85bceec78b1230dd1ee9a8976ac2e5
Ryan Mohta <ryanmohta@gmail.com>
2021-08-12 02:45:38 -0700

    Removed everything except the main file

--- spec/atom-auto-lightdark-mode-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomAutoLightdarkModeView from '../lib/atom-auto-lightdark-mode-view';
-
-describe('AtomAutoLightdarkModeView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
